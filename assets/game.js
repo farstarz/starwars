44162df79f99504c3a3bdde873ea82bd93bf50ea
Farstarz <vaibhav.pandey9890@gmail.com>
2018-09-19 17:22:03 -0700

    added score bar and loss win logic

--- assets/game.js
+++ assets/game.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
             enemy.healthPoints=enemy.healthPoints-hero.attackPower;
             hero.attackPower=hero.attackPower+hero.counterAttackPower;
             hero.attackNumber++;
-            var rate = 300;
+            var rate = 200;
             var i = 0;
             var firing1 = setInterval(function(){$("#fireRange1").html("<img src=\"./assets/images/strike1.PNG\" alt=\"strike1\" class=\"weapon\">");
 
@@ -27,14 +27,18 @@ $(document).ready(function(){
             var firing2 = setTimeout(function(){$("#fireRange1").html("<img src=\"./assets/images/strike2.PNG\" alt=\"strike2\" class=\"weapon\">");
 
               i++; console.log(i*100);},rate);
+              // empty range
+              setTimeout(function(){$("#fireRange1").empty();},2*rate-20);
             },rate*2);
 
             var firing3 = setInterval(function(){$("#fireRange2").html("<img src=\"./assets/images/strike1R.PNG\" alt=\"strike1R\" class=\"weapon\">");
 
-            // i++; console.log(i);
-            var firing4 = setTimeout(function(){$("#fireRange2").html("<img src=\"./assets/images/strike2R.PNG\" alt=\"strike2R\" class=\"weapon\">");
-
-            i++; console.log(i*100);},rate);
+              // i++; console.log(i);
+              var firing4 = setTimeout(function(){$("#fireRange2").html("<img src=\"./assets/images/strike2R.PNG\" alt=\"strike2R\" class=\"weapon\">");
+                // i++,; console.log(i*100);
+              },rate);
+              // empty range
+              setTimeout(function(){$("#fireRange2").empty();},2*rate-20);
             },rate*2);
             // function fire(){
             //   $("#fireRange").html("<h2>"+i+"</h2>");
@@ -47,7 +51,12 @@ $(document).ready(function(){
               // $("#fireRange").delay(2*time).empty();
                  
             
-            setTimeout(function(){clearInterval(firing1);clearTimeout(firing2);clearInterval(firing3);clearTimeout(firing4);}, 5000);
+            setTimeout(function(){
+              clearInterval(firing1);
+              $("#fireRange1").empty();
+              clearInterval(firing3);
+              $("#fireRange2").empty();
+            }, 2000);
             
             // $("#weapon").css({});
             // $("#fireRange").animate({left: "70px"}, 5000);
@@ -77,33 +86,44 @@ $(document).ready(function(){
             this.role="default";
             this.attackPower= this.counterAttackPower;
             this.attackNumber=0;
+            gameStart = false;
         }
     };
     
+    var gameStart = false;
     let hero = new character;
 	  let enemy = new character;
-    let harry = new character("Harry Potter", 120, "\"./assets/images/picL1.png\"", "\"./assets/images/picR1.png\"");
-    let hermionie = new character("Hermionie Granger", 100, "\"./assets/images/hermionieL.png\"", "\"./assets/images/hermionieR.png\"");
-    let luna = new character("Luna Lovegood", 150, "\"./assets/images/LunaL.png\"", "\"./assets/images/LunaR.png\"");
-    let gilderoy = new character("Gilderoy Lockhart", 180, "\"./assets/images/gilderockL.png\"", "\"./assets/images/gilderockR.png\"");
-    let snape = new character("Severus Snape", 180, "\"./assets/images/snapeL.png\"", "\"./assets/images/snapeR.png\"");
+    let harry = new character("Harry Potter", 140, "\"./assets/images/picL1.png\"", "\"./assets/images/picR1.png\"");
+    let hermionie = new character("Hermionie Granger", 150, "\"./assets/images/hermionieL.png\"", "\"./assets/images/hermionieR.png\"");
+    let luna = new character("Luna Lovegood", 100, "\"./assets/images/LunaL.png\"", "\"./assets/images/LunaR.png\"");
+    let gilderoy = new character("Gilderoy Lockhart", 120, "\"./assets/images/gilderockL.png\"", "\"./assets/images/gilderockR.png\"");
+    let snape = new character("Severus Snape", 500, "\"./assets/images/snapeL.png\"", "\"./assets/images/snapeR.png\"");
     // console.log(harry);
     // console.log(hermionie);
     // console.log(luna);
 		// console.log(gilderoy);
 		$(".role").on("click", function(){chooseHeroRole=this.value;})
-	
+    function updateScore(){
+      if(hero.healthPoints>enemy.healthPoints && enemy.healthPoints<0){
+        $("#scores").empty().html("<h2>"+hero.name+" Wins!</h2>");
+      }else if(hero.healthPoints<enemy.healthPoints && hero.healthPoints<0){
+        $("#scores").empty().html("<h2>"+enemy.name+" Wins!</h2>");
+      }else{
+        $("#scores").html("<h4>"+hero.name+": "+hero.healthPoints+"</h4> <h4 class=\"scoresR\">"+enemy.name+": "+enemy.healthPoints+"</h4>");
+      };
+    }
 		$("#hp").on("click", function(){ harry.chooseRole();});
 		$("#hg").on("click", function(){ hermionie.chooseRole();});
 		$("#ll").on("click", function(){ luna.chooseRole();});
 		$("#gl").on("click", function(){ gilderoy.chooseRole();});
     $("#sn").on("click", function(){ snape.chooseRole();});
-
+    $("#start").on("click", function(){ gameStart = true;})
 		$("#attack").on("click", function(){
 			hero.attack(enemy);
 			enemy.counterAttack(hero);
 			console.log(hero);
-			console.log(enemy);
+      console.log(enemy);
+      updateScore();
 		});
 		
 })
